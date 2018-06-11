1d3fc09e020fe10e7a785fea6a515ac001e3ff28
Consalv0 <gonzaloceciliano@gmail.com>
2018-06-11 08:28:24 -0500

    Removed FlexBison source

--- FlexBision.bat
+++ FlexBision.bat
@@ -1,4 +1,4 @@
 
 cd ./ExternalLibs/FlexBision
-win_flex.exe --outfile="..\..\parser.flex.cpp" --header-file="..\..\parser.flex.h" --wincompat  "parser.l"
-win_bison.exe --output="..\..\parser.tab.cpp" --defines="..\..\parser.tab.h"  "parser.y"
+win_flex.exe --outfile="..\..\BackpackString\source\parser.flex.cpp" --header-file="..\..\BackpackString\include\parser.flex.h" --wincompat  "parser.l"
+win_bison.exe --output="..\..\BackpackString\source\parser.tab.cpp" --defines="..\..\BackpackString\include\parser.tab.h"  "parser.y"
